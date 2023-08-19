a44e7c5f6d05f9a2a2194359356735531366690c
SHGroup <kshgroup@kshgroup.kr>
2023-08-19 16:52:10 +0900

    feat: 유저 수 세는 DB 체크용 임시 엔드포인트 추가

--- api/user.py
+++ api/user.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
 
+from db import db
+
 router = APIRouter()
+
+user = db.user
 
 
 @router.get("")
-def get_user():
+async def get_user():
     return {"user": "unknown"}
+
+
+@router.get("/count")
+async def count_user():
+    return {"cnt": await user.count_documents({})}
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from api import user_router
 
 app = FastAPI()
-
 app.include_router(user_router, prefix="/user")
 
 
